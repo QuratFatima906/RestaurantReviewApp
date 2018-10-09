6328414151e72c5ebb4ef7517cdaa0abafc6cb86
Qurat Ul Ain Fatima <quratfatima581@gmail.com>
2018-10-09 09:12:48 +0500

    2nd Review fixed

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -193,3 +193,22 @@ getParameterByName = (name, url) => {
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+// Register A service worker for offline caching
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('./sw.js')
+  .then((reg) => {
+    if(reg.installing){
+      console.log('Service worker installing');
+    }
+    else if(reg.waiting){
+      console.log('Service Worker installed');
+    }
+    else if(reg.active){
+      console.log('Service Worker active');
+    }
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((error) => {
+    console.log('Registration failed with ' + error);
+  });
+}
